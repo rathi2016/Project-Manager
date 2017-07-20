0b4a5440f3edf9af4b9f2a172c0110aee9047fde
Rathi <github email address>
2017-07-20 12:41:07 -0400

    added few stuffs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      projects:[
+        {
+          title: "Buisness Website",
+          category: "Web Design"
+        },
+        {
+          title: "Social App",
+          category: "Mobile Development"
+        },
+        {
+          title: "Ecommerse Shopping Cart",
+          category: "Web Design"
+        },
+      ]
+    }
+
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        My app
+        <Projects projects ={this.state.projects}/>
       </div>
     );
   }
